8ff573e76b7ac4233ec1e5d4c3901665bdc0259d
illtron3030 <illtron3030@gmail.com>
2022-07-25 21:10:30 -0700

    spacing

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 var pwdCriteria = {
   pwdLength: 0,
-  pwdLowerCase: ["a","b","c","d","e","f", "g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w", "x","y","z",],
+  pwdLowerCase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",],
   pwdUpperCase: ["A","B","C","D","E","F","G","H","I","J","K","L", "M","N", "O","P","Q","R","S","T","U","V","W","X","Y","Z",],
-  pwdNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  pwdNumber: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
   pwdCharacter: [
   '@',
   '%',
@@ -100,7 +100,6 @@ function generatePassword() {
                 result = result + uc;
                 pwdCriteria.pwdLength++;
               }
-            
             }
           }
         }
